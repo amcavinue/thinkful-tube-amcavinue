d71887d98e304b76e400ae0c21a95526492b212e
amcavinue <aaronmcavinue@gmail.com>
2016-10-17 09:16:55 -0400

    Implement AJAX.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
 $(function () {
     'use strict';
 
-    $.ajax();
+    $('form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            method: "GET",
+            url: "https://www.googleapis.com/youtube/v3/search",
+            data: {
+                part: 'snippet',
+                key: 'AIzaSyA0lb_D0ogHiJCwqsMStWjwQSM0ex8OxbY',
+                q: $('#js-searchbox').val()
+            }
+        }).done(function (data) {
+            $('#js-searchbox').val('');
+            console.log(data);
+        }).fail(function (data) {
+            console.log('failed');
+        });
+    });
 });
