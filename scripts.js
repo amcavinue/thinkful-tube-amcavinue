997ac32c2f414f693a03a4ff6e8e995c3b8ec999
amcavinue <aaronmcavinue@gmail.com>
2016-10-17 10:18:25 -0400

    Fin project

--- scripts.js
+++ scripts.js
@@ -13,9 +13,24 @@ $(function () {
             }
         }).done(function (data) {
             $('#js-searchbox').val('');
+            $('.js-results').empty();
+
+            for (var i = 0; i < data.items.length; i++) {
+                $('.js-results').append(
+                    '<li>' +
+                    '<a href="https://www.youtube.com/watch?v=' + data.items[i].id.videoId + '"><img src="' + data.items[i].snippet.thumbnails.default.url + '" />'+
+                    '<span class="title">' + data.items[i].snippet.description.substring(0, 75) + '...' + '</span>'+
+                    '</a></li>'
+                );
+            }
+
             console.log(data);
         }).fail(function (data) {
             console.log('failed');
         });
     });
 });
+
+function renderLi() {
+    return;
+}
